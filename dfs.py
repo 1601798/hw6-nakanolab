590cea5532cb17ff1f890a757598c21b051317c5
Jun Nakano <nakanoj@gmail.com>
2018-12-21 11:54:30 +0900

    Modified dfs to return all paths.

--- dfs.py
+++ dfs.py
@@ -1,4 +1,5 @@
 def dfs(problem):
+    routes = []
     found = {problem.get_start_state()}
     stack = [[problem.get_start_state()]]
     while stack:
@@ -6,7 +7,8 @@ def dfs(problem):
         u = path[-1]  # path の最後のノード
         for v in problem.next_states(u):
             if problem.is_goal(v):
-                return path + [v]
+                routes.append(path + [v])
             elif v not in found:
                 found.add(v)
                 stack.append(path + [v])
+    return routes
